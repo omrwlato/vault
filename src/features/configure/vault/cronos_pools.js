1a53e9201823b75fed5cc60b727820b3513757b5
rondadefidon <flyfreshfits@gmail.com>
2022-05-20 22:10:37 -0400

    update cronos chain

--- src/features/configure/vault/cronos_pools.js
+++ src/features/configure/vault/cronos_pools.js
@@ -1,1 +1,38 @@
-export const cronosPools = [];
+export const cronosPools = [
+    {
+        id: 'toxic-deer-usdc',
+        name: 'DEER-USDC',
+        token: 'DEER-USDC',
+        tokenDescription: 'ToxicDeer Finance',
+        tokenAddress: '0x264f27BF0eC4fE383Cfda50F1bb11588735bbE6D',
+        tokenDecimals: 18,
+        tokenDescriptionUrl: '#',
+        earnedToken: 'froyotoxicDEER-USDC',
+        earnedTokenAddress: '0xAE21d31a6494829a9E4B2B291F4984AAE8121757',
+        earnContractAddress: '0xAE21d31a6494829a9E4B2B291F4984AAE8121757',
+        pricePerFullShare: 1,
+        tvl: 0,
+        oracle: 'lps',
+        oracleId: 'toxic-deer-usdc',
+        oraclePrice: 0,
+        depositsPaused: false,
+        status: 'active',
+        platform: 'Other',
+        assets: ['DEER', 'USDC'],
+        risks: [
+          'COMPLEXITY_LOW',
+          'BATTLE_TESTED',
+          'IL_LOW',
+          'MCAP_MICRO',
+          'PLATFORM_ESTABLISHED',
+          'CONTRACTS_VERIFIED',
+        ],
+        stratType: 'StratLP',
+        addLiquidityUrl:
+          'https://mm.finance/add/0xc21223249ca28397b4b6541dffaecc539bff0c59/0x6D6D6ba0c7e7DBaFffeC82b1ddF92e271650a63A',
+        buyTokenUrl:
+          'https://mm.finance/swap?inputCurrency=0xc21223249CA28397B4B6541dfFaEcC539BfF0c59&outputCurrency=0x6D6D6ba0c7e7DBaFffeC82b1ddF92e271650a63A',
+        family: 'ALL',
+        createdAt: 1650758400,
+      }
+];
